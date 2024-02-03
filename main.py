22b803d5d2537d7e53d9f9de9f86406b1a5c94af
Bruno Campos Fonseca <92186023+brunocamposfonseca@users.noreply.github.com>
2024-02-03 01:54:31 -0300

    eva

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-'''
-import speech_recognition as sr
-
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    while True:
-        audio = r.listen(source) 
-
-        print(r.recognize_google(audio, language='pt'))
-
-'''
-
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import pyttsx3
+import json
+
+# Síntese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
@@ -27,8 +26,11 @@ while True:
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text = result['text']
+
+            print(text)
+            speak(text)
